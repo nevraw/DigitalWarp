5783e38ccd0becdac3b012a1f4480c758cdebeac
nevraw <nevraw@users.noreply.github.com>
2015-11-07 23:44:39 +0000

    Update main_bw.js

--- main_bw.js
+++ main_bw.js
@@ -23,18 +23,6 @@ function buttonHandler() {
  });
 }
 
-// Radio control for hiding battery
-var $hideBattery;
-$("input[name=hideBattery]").change(function () {
- $hideBattery = parseInt(this.value);
-});
-
-// Radio control for animation on shake
-var $noAnimOnShake;
-$("input[name=noAnimOnShake]").change(function () {
- $noAnimOnShake = parseInt(this.value);
-});
-
 // Radio control for inverting colors
 var $invert;
 $("input[name=$invert]").change(function () {
@@ -43,26 +31,6 @@ $("input[name=$invert]").change(function () {
 
 
 function loadOptions() {
- if (localStorage.hideBattery) {
-  $hideBattery = localStorage.hideBattery;
-//  console.log('localStorage.hideBattery: ' + $hideBattery);
-  // setting radio' value
- } else {
-  $hideBattery = 0;
-//  console.log('localStorage.hideBattery was undefined, now set to: ' + $hideBattery);
- }
- $("input[name=hideBattery][value='" + $hideBattery + "']").attr('checked', 'checked');
-
- if (localStorage.noAnimOnShake) {
-  $noAnimOnShake = localStorage.noAnimOnShake;
-//  console.log('localStorage.noAnimOnShake: ' + $noAnimOnShake);
-  // setting radio' value
- } else {
-  $noAnimOnShake = 0;
-//  console.log('localStorage.noAnimOnShake was undefined, now set to: ' + $noAnimOnShake);
- }
- $("input[name=noAnimOnShake][value='" + $noAnimOnShake + "']").attr('checked', 'checked');
-
  if (localStorage.invert) {
   $invert = localStorage.invert;
 //  console.log('localStorage.invert: ' + $invert);
@@ -78,15 +46,11 @@ function loadOptions() {
 function getAndStoreConfigData() {
 
  var options = {
-  hideBattery:   $hideBattery,
-  noAnimOnShake: $noAnimOnShake,
   invert:        $invert
  };
  
  console.log('Got options: ' + JSON.stringify(options));
 
- localStorage.hideBattery   = $hideBattery;
- localStorage.noAnimOnShake = $noAnimOnShake;
  localStorage.invert        = $invert;
 
  return options;
