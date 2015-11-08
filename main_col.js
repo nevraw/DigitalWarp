2d0691f460c8c6c68f80568b97cfdda28a106584
nevraw <nevraw@users.noreply.github.com>
2015-11-08 14:18:04 +0000

    Update main_col.js

--- main_col.js
+++ main_col.js
@@ -42,6 +42,12 @@ function getAndStoreConfigData() {
  var $timeColorPicker = $('#timeColorPicker');
  var $bgColorPicker = $('#bgColorPicker');
 
+
+ if ($bgcolorPicker == $timecolorPicker) {
+  alert('Please select different colors for time and background');
+  return
+ }
+
  var options = {
   timeColor:     $timeColorPicker.val(),
   bgColor:       $bgColorPicker.val()
